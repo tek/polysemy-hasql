974b9200d61e611bb760fd2b0fbd9db99b4b5a5a
Torsten Schmits <torstenschmits@gmail.com>
2021-03-31 01:07:05 +0200

    auto version of atomic state interpreter

--- packages/hasql/integration/Polysemy/Hasql/Test/AtomicState.hs
+++ packages/hasql/integration/Polysemy/Hasql/Test/AtomicState.hs
@@ -3,9 +3,7 @@ module Polysemy.Hasql.Test.AtomicState where
 import Polysemy.Db.Data.DbError (DbError)
 import Polysemy.Test (UnitTest, (===))
 
-import Polysemy.Hasql.AtomicState (interpretAtomicStateDb)
-import Polysemy.Hasql.ManagedTable (interpretManagedTableAuto)
-import Polysemy.Hasql.Query (interpretQueryAuto)
+import Polysemy.Hasql.AtomicState (interpretAtomicStateDbAuto)
 import Polysemy.Hasql.Test.Run (integrationTest)
 
 data Cat =
@@ -18,7 +16,7 @@ data Cat =
 test_atomicStateDb :: UnitTest
 test_atomicStateDb =
   integrationTest do
-    r <- interpretManagedTableAuto $ interpretQueryAuto $ interpretAtomicStateDb (Cat 5 "fuzzyboots") do
+    r <- interpretAtomicStateDbAuto (Cat 5 "fuzzyboots") do
       restop @DbError do
         atomicModify' \ (Cat _ nam) -> Cat 200 [qt|mr. #{nam}|]
         atomicGet
--- packages/hasql/lib/Polysemy/Hasql/AtomicState.hs
+++ packages/hasql/lib/Polysemy/Hasql/AtomicState.hs
@@ -1,13 +1,23 @@
 module Polysemy.Hasql.AtomicState where
 
 import Polysemy.Db.AtomicState (interpretAtomicStateStore)
+import Polysemy.Db.Data.Column (Auto)
+import Polysemy.Db.Data.DbError (DbError)
 import Polysemy.Db.Data.InitDbError (InitDbError)
+import Polysemy.Log (Log)
 
 import Polysemy.Hasql.Crud (interpretCrudSingleton)
+import Polysemy.Hasql.Data.Database (Database)
 import Polysemy.Hasql.Data.ManagedTable (ManagedTable)
 import Polysemy.Hasql.Data.Query (Query)
+import Polysemy.Hasql.ManagedTable (interpretManagedTableAuto)
+import Polysemy.Hasql.Query (interpretQueryAuto)
 import Polysemy.Hasql.Store (interpretStoreDb)
+import Polysemy.Hasql.Table.QueryTable (GenQueryTable)
 
+-- |Interpret 'AtomicState' as a singleton table.
+--
+-- Given an initial value, every state action reads the value from the database and writes it back.
 interpretAtomicStateDb ::
   Show e =>
   Members [Query () d, ManagedTable d !! e, Error InitDbError] r =>
@@ -19,3 +29,20 @@ interpretAtomicStateDb initial =
   interpretAtomicStateStore initial .
   raiseUnder2
 {-# INLINE interpretAtomicStateDb #-}
+
+-- |Interpret 'AtomicState' as a singleton table.
+--
+-- Given an initial value, every state action reads the value from the database and writes it back.
+--
+-- Uses the automatic derivation strategy.
+interpretAtomicStateDbAuto ::
+  GenQueryTable Auto Auto () d =>
+  Members [Database !! DbError, Error InitDbError, Log, Embed IO] r =>
+  d ->
+  InterpreterFor (AtomicState d !! DbError) r
+interpretAtomicStateDbAuto initial =
+  interpretQueryAuto .
+  interpretManagedTableAuto .
+  interpretAtomicStateDb initial .
+  raiseUnder2
+{-# INLINE interpretAtomicStateDbAuto #-}
