59f4ac0be62c1bd66e44e2a052207a2396124f56
Torsten Schmits <torstenschmits@gmail.com>
2021-04-14 02:51:43 +0200

    instead of following, use transitive deps via `input.`

--- flake.nix
+++ flake.nix
@@ -2,21 +2,17 @@
   description = "Polysemy Effects for Databases";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
-    tryp-hs = {
-      url = github:tek/tryp-hs;
-      # url = path:/home/tek/code/tek/nix/tryp-hs;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    polysemy-test.url = github:tek/polysemy-test;
-    polysemy-time.url = github:tek/polysemy-time;
-    polysemy-conc.url = github:tek/polysemy-conc;
     polysemy-log.url = github:tek/polysemy-log;
     polysemy-resume.url = github:tek/polysemy-resume;
   };
 
-  outputs = { tryp-hs, polysemy-test, polysemy-time, polysemy-conc, polysemy-log, polysemy-resume, ... }:
+  outputs = { polysemy-log, polysemy-resume, ... }:
   let
+    inherit (polysemy-log.inputs) polysemy-conc;
+    inherit (polysemy-conc.inputs) polysemy-time;
+    inherit (polysemy-time.inputs) polysemy-test chronos;
+    inherit (polysemy-test.inputs) tryp-hs;
+
     common = { hackage, source, jailbreak, ... }: {
       fcf-containers = jailbreak (hackage "0.6.0" "0wxc5213dcxkmd2j1vkhjqsqsxipv8hbq3jnc0ll4xzrlpqic3wf");
       polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
@@ -25,7 +21,7 @@
       co-log-polysemy = jailbreak (hackage "0.0.1.2" "17bcs8dvrhwfcyklknkqg11gxgxm2jaa7kbm6xx4vm1976abzwss");
     };
 
-    overrides = { hackage, source, jailbreak, ... }@args: common args // {
+    main = { hackage, source, jailbreak, ... }: {
       path = hackage "0.8.0" "0isldidz2gypw2pz399g6rn77x9mppd1mvj5h6ify4pj4mpla0pb";
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
@@ -36,7 +32,7 @@
       polysemy-resume = source.sub polysemy-resume "packages/resume";
     };
 
-    compatOverrides = { hackage, source, versions, jailbreak, ... }@args: common args // {
+    compat = { hackage, source, versions, jailbreak, ... }: {
       polysemy-test = hackage "0.3.1.1" "0x0zg1kljr7a1mwmm3zrmha5inz3l2pkldnq65fvsig8f3x8rsar";
       polysemy-time = hackage "0.1.2.1" "09l8r5fx0vnapdn8p0cwiwprgg3i67m58dd4j5hcdhw34gfqnnsr";
       polysemy-conc = hackage "0.1.0.2" "0ijz5l8q53d1s7i100gvjdhzv80dpd140m7a9hyam113ybglc8lg";
@@ -52,10 +48,9 @@
   in
   tryp-hs.flake {
     base = ./.;
-    compiler = "ghc8104";
     main = "polysemy-hasql";
-    overrides = tryp-hs.overrides overrides;
-    compatOverrides = tryp-hs.overrides compatOverrides;
+    overrides = tryp-hs.composeCabal [common main];
+    compatOverrides = tryp-hs.composeCabal [common compat];
     packages = {
       polysemy-db = "packages/db";
       polysemy-db-data = "packages/data";
