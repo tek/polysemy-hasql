4de9a9feb3c32983af9b7d010d985ddc490e43b1
Torsten Schmits <git@tryp.io>
2022-12-22 20:25:33 +0100

    add tests for higher-order Dd assembly

--- packages/sqel/test/Sqel/Test/StatementTest.hs
+++ packages/sqel/test/Sqel/Test/StatementTest.hs
@@ -2,40 +2,71 @@
 
 module Sqel.Test.StatementTest where
 
+import Data.Type.Equality ((:~:) (Refl))
+import Generics.SOP (NP (..))
 import Hedgehog (TestT, (===))
 import Prelude hiding (sum)
 import Test (unitTest)
 import Test.Tasty (TestTree, testGroup)
 
-import Sqel.Class.MatchView (MatchViewPath)
-import Sqel.Combinators (Merge)
-import Sqel.Data.Codec (FullCodec)
-import Sqel.Data.Dd (DbTypeName (dbTypeName), Dd, DdK (DdK), DdType, MatchDdType, type (:>) ((:>)))
-import Sqel.Data.FieldPath (FieldPath (FieldPath), FieldPathsSub)
+import Sqel.Class.MatchView (HasColumn)
+import Sqel.Data.Dd (
+  DbTypeName (dbTypeName),
+  Dd (Dd),
+  DdInc (DdNest),
+  DdK (DdK),
+  DdStruct (DdComp),
+  DdType,
+  DdTypeName,
+  DdVar (DdProd),
+  Struct (Comp),
+  type (:>) ((:>)),
+  )
+import Sqel.Data.Mods (pattern NoMods)
 import Sqel.Data.Order (Order (Desc))
 import Sqel.Data.QuerySchema (QuerySchema)
+import Sqel.Data.Sel (MkSel (mkSel), Sel (SelSymbol), SelW (SelWAuto))
 import Sqel.Data.Select (Select (Select))
 import Sqel.Data.Sql (Sql, sql, toSql)
 import Sqel.Data.TableSchema (TableSchema)
-import Sqel.Data.Term (DdTerm)
 import Sqel.Data.Uid (Uid)
-import Sqel.Fold (FoldDd)
-import Sqel.Merge (merge)
-import Sqel.Names.Amend (AmendName)
-import Sqel.Names.Rename (Rename)
-import Sqel.PgType (tableSchema)
-import Sqel.Prim (prim, prims)
+import Sqel.Merge (Merge, merge)
+import Sqel.PgType (MkTableSchema, tableSchema)
+import Sqel.Prim (prim, primAs, prims)
 import Sqel.Product (prod)
 import Sqel.Query (checkQuery)
 import Sqel.Query.Combinators (order)
-import Sqel.ReifyCodec (ReifyCodec)
-import Sqel.ReifyDd (FoldComp, FoldPrim)
 import qualified Sqel.Sql.Select as Sql
 import Sqel.Sum (con, con1, sum)
-import Sqel.Uid (uid)
+import qualified Sqel.Type as T
+import Sqel.Type (Prim, Prod, ProdPrims, TypeName, type (*>), type (>))
+import Sqel.Uid (UidDd, uid)
 
-data Prod =
-  Prod {
+data Three =
+  Three {
+    a :: Int,
+    b :: Int,
+    c :: Int
+  }
+  deriving stock (Eq, Show, Generic)
+
+type ThreeTableGen =
+  ProdPrims Three
+
+type ThreeTable =
+  Prod Three *> Prim "a" Int > Prim "b" Int > Prim "c" Int
+
+test_prodGen :: TestT IO ()
+test_prodGen =
+  case Refl :: ThreeTable :~: ThreeTableGen of
+    Refl -> unit
+
+ddThree :: Dd ThreeTableGen
+ddThree =
+  prod prims
+
+data Pro =
+  Pro {
     num :: Int64,
     name :: Text
   }
@@ -53,8 +84,11 @@ data Q =
   }
   deriving stock (Eq, Show, Generic)
 
-ddProd :: Dd ('DdK _ _ Prod _)
-ddProd =
+type ProdTable =
+  Prod Pro *> (Prim "num" Int64 > Prim "name" Text)
+
+ddPro :: Dd ProdTable
+ddPro =
   prod prims
 
 target_order :: Sql
@@ -95,7 +129,7 @@ test_statement_merge_sum = do
   target_mergeSum === toSql (Select (tableSchema dd_uid_merge_sum_manual))
 
 data MergeProd =
-  MergeProd { count :: Int, b :: Prod }
+  MergeProd { count :: Int, b :: Pro }
   deriving stock (Eq, Show, Generic)
 
 dd_merge_prod :: Dd ('DdK _ _ MergeProd _)
@@ -117,62 +151,77 @@ data Wrap a =
 instance DbTypeName a name => DbTypeName (Wrap a) name where
   dbTypeName = dbTypeName @a
 
-data Merge1 =
-  One { one :: Prod }
-  |
-  Two { two :: Prod }
-  deriving stock (Eq, Show, Generic)
+type WrapDd sa =
+  TypeName (DdTypeName sa) (Prod (Wrap (DdType sa))) *> (
+    T.Merge sa >
+    Prim "length" Int64
+  )
 
-data QHo =
-  QHo { name :: Text }
-  deriving stock (Eq, Show, Generic)
-
-data QWrap =
-  QWrap { two :: QHo }
-  deriving stock (Eq, Show, Generic)
-
-ddWrap ::
-  ∀ a s0 s1 name .
-  DdType s0 ~ a =>
-  MatchDdType s1 a =>
-  KnownSymbol name =>
-  DbTypeName a name =>
-  Merge a s0 s1 =>
-  Rename s1 (AmendName s1 "wrapped") =>
+schema_higherOrder ::
+  ∀ s0 table a sel mods s .
+  s0 ~ 'DdK sel mods a s =>
+  table ~ WrapDd s0 =>
+  Merge a s0 (T.Merge s0) =>
+  MkSel ('SelSymbol (DdTypeName s0)) =>
+  MkTableSchema table =>
   Dd s0 ->
-  Dd ('DdK _ _ (Uid Int64 (Wrap a)) _)
-ddWrap wrapped =
-  uid prim (prod (merge @a wrapped :> prim))
+  TableSchema (Wrap a)
+schema_higherOrder wrapped =
+  tableSchema dd
+  where
+    dd :: Dd table
+    dd = Dd SelWAuto NoMods (DdComp mkSel DdProd DdNest fields)
+    fields = merge @a wrapped :* primAs @"length" :* Nil
 
-ddMerge1 :: Dd ('DdK _ _ Merge1 _)
-ddMerge1 = sum (con1 ddProd :> con1 ddProd)
+target_higherOrder :: Sql
+target_higherOrder =
+  [sql|select "num", "name", "length" from "pro"|]
 
-target_merge_query_higherOrder :: Sql
-target_merge_query_higherOrder =
-  [sql|select "id", "ph_sum_index__merge1", ("one").num, ("one").name, ("two").num, ("two").name, "length"
-       from "merge1" where ((("two")."name" = $1))|]
+test_statement_higherOrder :: TestT IO ()
+test_statement_higherOrder =
+  target_higherOrder === toSql (Select (schema_higherOrder ddPro))
 
 statement_query_higherOrder ::
-  ∀ a s0 s1 s2 name .
-  MatchDdType s1 a =>
-  KnownSymbol name =>
-  DbTypeName a name =>
-  Merge a s0 s1 =>
-  s2 ~ AmendName s1 "wrapped" =>
-  Rename s1 (AmendName s1 "wrapped") =>
-  ReifyCodec FullCodec s2 a =>
-  FoldDd FoldPrim FoldComp DdTerm s2 =>
-  MatchViewPath ('FieldPath ["two", "name"] Text) ((FieldPathsSub '[] s2 ++ '[ 'FieldPath '["length"] Int64 ]) ++ '[]) 'True =>
+  ∀ a s0 table sel mods tsel c i s .
+  s0 ~ 'DdK sel mods a ('Comp ('SelSymbol tsel) c i s) =>
+  table ~ UidDd (Prim "id" Int64) (WrapDd s0) =>
+  KnownSymbol tsel =>
+  DbTypeName a tsel =>
+  MkTableSchema table =>
+  HasColumn ["two", "name"] Text table =>
   Dd s0 ->
   Sql
-statement_query_higherOrder wrapped =
+statement_query_higherOrder wrapped@(Dd _ _ (DdComp tsel _ _ _)) =
   Sql.selectWhere qs ts
   where
     ts :: TableSchema (Uid Int64 (Wrap a))
     ts = tableSchema dd
     qs :: QuerySchema QWrap (Uid Int64 (Wrap a))
     qs = checkQuery (prod (prod prim)) dd
-    dd = uid prim (prod (merge @a wrapped :> prim))
+    dd :: Dd table
+    dd = uid prim pro
+    pro = Dd SelWAuto NoMods (DdComp tsel DdProd DdNest fields)
+    fields = merge wrapped :* primAs @"length" :* Nil
+
+data Merge1 =
+  One { one :: Pro }
+  |
+  Two { two :: Pro }
+  deriving stock (Eq, Show, Generic)
+
+data QHo = QHo { name :: Text }
+  deriving stock (Eq, Show, Generic)
+
+data QWrap = QWrap { two :: QHo }
+  deriving stock (Eq, Show, Generic)
+
+ddMerge1 :: Dd ('DdK _ _ Merge1 _)
+ddMerge1 = sum (con1 ddPro :> con1 ddPro)
+
+target_merge_query_higherOrder :: Sql
+target_merge_query_higherOrder =
+  [sql|select "id", "ph_sum_index__merge1", ("one").num, ("one").name, ("two").num, ("two").name, "length"
+       from "merge1" where ((("two")."name" = $1))|]
 
 test_statement_merge_query_higherOrder :: TestT IO ()
 test_statement_merge_query_higherOrder =
@@ -184,5 +233,6 @@ test_statement =
     unitTest "order" test_statement_order,
     unitTest "merge sum" test_statement_merge_sum,
     unitTest "merge prod" test_statement_merge_prod,
-    unitTest "higher-order merge query" test_statement_merge_query_higherOrder
+    unitTest "higher-order merge statement" test_statement_higherOrder,
+    unitTest "higher-order double merge query" test_statement_merge_query_higherOrder
   ]
