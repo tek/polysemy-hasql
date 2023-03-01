e5ce40e09dbe749d315c378e4b8943671ba96211
Torsten Schmits <git@tryp.io>
2023-03-01 22:43:39 +0100

    use newtype strategy for deriving Read on DbPort

--- packages/db/lib/Polysemy/Db/Data/DbPort.hs
+++ packages/db/lib/Polysemy/Db/Data/DbPort.hs
@@ -2,8 +2,8 @@ module Polysemy.Db.Data.DbPort where
 
 newtype DbPort =
   DbPort { unDbPort :: Int }
-  deriving stock (Eq, Show, Read, Generic)
-  deriving newtype (Num, Real, Enum, Integral, Ord)
+  deriving stock (Eq, Show, Generic)
+  deriving newtype (Read, Num, Real, Enum, Integral, Ord)
 
 instance Default DbPort where
   def = 5432
