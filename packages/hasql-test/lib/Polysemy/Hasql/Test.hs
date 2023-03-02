7660238a403f6256f91a6f71ddd3483ab42f9a2a
Torsten Schmits <git@tryp.io>
2023-03-02 21:52:07 +0100

    export migration test functions

--- packages/hasql-test/lib/Polysemy/Hasql/Test.hs
+++ packages/hasql-test/lib/Polysemy/Hasql/Test.hs
@@ -1,5 +1,7 @@
 module Polysemy.Hasql.Test (
   module Polysemy.Hasql.Test.Run,
+  module Polysemy.Hasql.Test.Migration,
 ) where
 
 import Polysemy.Hasql.Test.Run (integrationTest, integrationTestWith, runIntegrationTestWith, dbConfig)
+import Polysemy.Hasql.Test.Migration
